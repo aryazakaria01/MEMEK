1c6f0bae40c501aec2dc0a07a558ea696709f1b5
Shohih Abdul <shohih242@gmail.com>
2021-10-03 21:15:32 +0700

    fix bug

--- triplesix/handlers/stream.py
+++ triplesix/handlers/stream.py
@@ -94,12 +94,12 @@ async def stream_v2(_, message: Message):
 
 @Client.on_message(command("playlist"))
 async def get_playlist(_, message: Message):
+    playlist = player.playlist
     if not playlist:
         return await message.reply("No playlist")
     chat_id = message.chat.id
     if not player.call.get_call(chat_id):
         return await message.reply("Not playing")
-    playlist = player.playlist
     current = playlist[chat_id][0]
     ples = "\n"
     j = 0
