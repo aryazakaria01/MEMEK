617fb56fffa355abffc44b77cae54c09ba7e7ffd
Shohih Abdul <shohih242@gmail.com>
2021-10-03 21:13:19 +0700

    fix update

--- triplesix/handlers/stream.py
+++ triplesix/handlers/stream.py
@@ -94,15 +94,19 @@ async def stream_v2(_, message: Message):
 
 @Client.on_message(command("playlist"))
 async def get_playlist(_, message: Message):
+    if not playlist:
+        return await message.reply("No playlist")
     chat_id = message.chat.id
+    if not player.call.get_call(chat_id):
+        return await message.reply("Not playing")
     playlist = player.playlist
     current = playlist[chat_id][0]
     ples = "\n"
     j = 0
-    for i in playlist[chat_id]:
+    for i in playlist[chat_id][1:]:
         j += 1
         query = i["query"]
         ples += f"{j}. {query}\n"
     await message.reply(
-        f"Current streaming\n{current}\n\nOn Playlist\n\n{ples}"
+        f"Current streaming\n{current['query']}\n\nOn Playlist\n\n{ples}"
     )
